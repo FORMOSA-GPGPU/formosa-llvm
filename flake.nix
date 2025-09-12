e18e56754401a81f453fa1e96ef22fdb9639019a
ping-ee <a91249061@gmail.com>
2025-09-12 13:25:44 +0800

    Flake update packages

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "LLVM compiler for FORMOSA GPGPU";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=release-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=release-25.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
