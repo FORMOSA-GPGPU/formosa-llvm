e581edcfa1193059fd317ea997ff9898454aef80
Meng-Tsung Tsai <hwahwa649@gmail.com>
2026-04-24 13:49:10 +0800

    Skip reconv point of loop headers

--- llvm/lib/Target/RISCV/RISCVFSAPostTopo.cpp
+++ llvm/lib/Target/RISCV/RISCVFSAPostTopo.cpp
@@ -5,6 +5,7 @@
 #include "llvm/CodeGen/MachineFunction.h"
 #include "llvm/CodeGen/MachineInstr.h"
 #include "llvm/CodeGen/MachineInstrBuilder.h"
+#include "llvm/CodeGen/MachineLoopInfo.h"
 #include "llvm/CodeGen/Passes.h"
 #include "llvm/InitializePasses.h"
 #include "llvm/ADT/PostOrderIterator.h"
@@ -12,13 +13,22 @@
 #include <unordered_set>
 using namespace llvm;
 #define DEBUG_TYPE "RISCVFSAPostTopo"
+extern cl::opt<bool> FSASkipLoopHeader;
 namespace {
 class RISCVFSAPostTopo : public MachineFunctionPass {
 private:
   // Target Reg info
   const RISCVInstrInfo *TII;
+  MachineLoopInfo *MLI;
   MachineBasicBlock::iterator blockBeginInsertPt(MachineBasicBlock &MBB);
   bool shouldSkipInsertion(MachineBasicBlock &MBB);
+  std::unordered_set<MachineBasicBlock *> collectSkipLoopHeaders();
+  std::unordered_set<MachineBasicBlock *>
+  collectReconvBlocks(MachineFunction &MF,
+                      const std::unordered_set<MachineBasicBlock *> &SkipMBBs);
+  unsigned insertLocalPriorityPairs(MachineFunction &MF,
+                                    const std::unordered_set<MachineBasicBlock *> &ReconvBBs);
+  void insertEntryExitCompensation(MachineFunction &MF, unsigned Pri);
   void insertLower(MachineBasicBlock &MBB, unsigned Pri);
   void insertRaise(MachineBasicBlock &MBB, unsigned Pri);
 
@@ -31,6 +41,7 @@ public:
     return "RISCVFSAPostTopo";
   }
   void getAnalysisUsage(AnalysisUsage &AU) const override {
+    AU.addRequired<MachineLoopInfoWrapperPass>();
     MachineFunctionPass::getAnalysisUsage(AU);
   }
 };
@@ -45,6 +56,7 @@ INITIALIZE_PASS_BEGIN(RISCVFSAPostTopo, DEBUG_TYPE,
                       "instructions based on post order dfs, use argument "
                       "-fsa-post-topo to enable the pass",
                       false, false)
+INITIALIZE_PASS_DEPENDENCY(MachineLoopInfoWrapperPass)
 INITIALIZE_PASS_END(RISCVFSAPostTopo, DEBUG_TYPE,
                       "FSA handling reconv priority by inserting fsa.pri "
                       "instructions based on post order dfs, use argument "
@@ -55,6 +67,7 @@ INITIALIZE_PASS_END(RISCVFSAPostTopo, DEBUG_TYPE,
 
 void RISCVFSAPostTopo::initialize(MachineFunction &MF) {
   const auto &ST = MF.getSubtarget<RISCVSubtarget>();
+  MLI = &getAnalysis<MachineLoopInfoWrapperPass>().getLI();
   TII = ST.getInstrInfo();
 }
 
@@ -64,6 +77,7 @@ RISCVFSAPostTopo::blockBeginInsertPt(MachineBasicBlock &MBB) {
 }
 
 bool RISCVFSAPostTopo::shouldSkipInsertion(MachineBasicBlock &MBB) {
+  // return false; // Do not skip by default
   auto CountUntilBarOrTerm = [this](MachineBasicBlock &Block, bool &HitBar) {
     unsigned Count = 0;
     HitBar = false;
@@ -96,6 +110,39 @@ bool RISCVFSAPostTopo::shouldSkipInsertion(MachineBasicBlock &MBB) {
   return Count <= 2;
 }
 
+std::unordered_set<MachineBasicBlock *>
+RISCVFSAPostTopo::collectSkipLoopHeaders() {
+  std::unordered_set<MachineBasicBlock *> SkipMBBs;
+  if (!FSASkipLoopHeader)
+    return SkipMBBs;
+
+  for (auto *Loop : *MLI) {
+    auto *MBB = Loop->getHeader();
+    if (MBB && MBB->pred_size() <= 2)
+      SkipMBBs.insert(MBB);
+
+    for (auto *SubLoop : *Loop) {
+      auto *SubMBB = SubLoop->getHeader();
+      if (SubMBB && SubMBB->pred_size() <= 2)
+        SkipMBBs.insert(SubMBB);
+    }
+  }
+  return SkipMBBs;
+}
+
+std::unordered_set<MachineBasicBlock *>
+RISCVFSAPostTopo::collectReconvBlocks(
+    MachineFunction &MF,
+    const std::unordered_set<MachineBasicBlock *> &SkipMBBs) {
+  std::unordered_set<MachineBasicBlock *> ReconvBBs;
+  for (MachineBasicBlock &MBB : MF) {
+    if (MBB.pred_size() > 1 && !SkipMBBs.count(&MBB) &&
+        !shouldSkipInsertion(MBB))
+      ReconvBBs.insert(&MBB);
+  }
+  return ReconvBBs;
+}
+
 void RISCVFSAPostTopo::insertLower(MachineBasicBlock &MBB, unsigned Pri) {
   auto InsertPt = blockBeginInsertPt(MBB);
   BuildMI(MBB, InsertPt, MBB.findDebugLoc(InsertPt),
@@ -108,6 +155,39 @@ void RISCVFSAPostTopo::insertRaise(MachineBasicBlock &MBB, unsigned Pri) {
   BuildMI(MBB, InsertPt, MBB.findDebugLoc(InsertPt),
           TII->get(RISCV::FSA_PRI_RAISE_N))
       .addImm(Pri);
+}
+
+unsigned RISCVFSAPostTopo::insertLocalPriorityPairs(
+    MachineFunction &MF,
+    const std::unordered_set<MachineBasicBlock *> &ReconvBBs) {
+  unsigned NextPri = 1;
+  for (MachineBasicBlock *MBB :
+       ReversePostOrderTraversal<MachineBasicBlock *>(&MF.front())) {
+    if (!ReconvBBs.count(MBB))
+      continue;
+    // Exit blocks only receive entry/exit compensation, not local reconv pairs.
+    if (MBB->succ_empty())
+      continue;
+    insertLower(*MBB, NextPri);
+    insertRaise(*MBB, NextPri);
+    ++NextPri;
+  }
+  return NextPri;
+}
+
+void RISCVFSAPostTopo::insertEntryExitCompensation(MachineFunction &MF,
+                                                   unsigned Pri) {
+  MachineBasicBlock &EntryMBB = MF.front();
+  auto EntryInsertPt = EntryMBB.getFirstTerminator();
+  BuildMI(EntryMBB, EntryInsertPt, EntryMBB.findDebugLoc(EntryInsertPt),
+          TII->get(RISCV::FSA_PRI_RAISE_N))
+      .addImm(Pri);
+
+  for (MachineBasicBlock &MBB : MF) {
+    if (!MBB.succ_empty())
+      continue;
+    insertLower(MBB, Pri);
+  }
 }
 
 bool RISCVFSAPostTopo::runOnMachineFunction(MachineFunction &MF) {
@@ -130,37 +210,16 @@ bool RISCVFSAPostTopo::runOnMachineFunction(MachineFunction &MF) {
     return false;
   }
 
-  std::unordered_set<MachineBasicBlock *> ReconvBBSet;
-  for (MachineBasicBlock &MBB : MF) {
-    if (MBB.pred_size() > 1 && !shouldSkipInsertion(MBB))
-      ReconvBBSet.insert(&MBB);
-  }
+  std::unordered_set<MachineBasicBlock *> SkipInsertMBBSet =
+      collectSkipLoopHeaders();
+  std::unordered_set<MachineBasicBlock *> ReconvBBSet =
+      collectReconvBlocks(MF, SkipInsertMBBSet);
 
   if (ReconvBBSet.empty())
     return false;
 
-  unsigned NextPri = 1;
-  for (MachineBasicBlock *MBB :
-       ReversePostOrderTraversal<MachineBasicBlock *>(&MF.front())) {
-    if (!ReconvBBSet.count(MBB))
-      continue;
-    insertLower(*MBB, NextPri);
-    insertRaise(*MBB, NextPri);
-    ++NextPri;
-  }
-
-  unsigned EntryExitPri = NextPri;
-  MachineBasicBlock &EntryMBB = MF.front();
-  auto EntryInsertPt = EntryMBB.getFirstTerminator();
-  BuildMI(EntryMBB, EntryInsertPt, EntryMBB.findDebugLoc(EntryInsertPt),
-          TII->get(RISCV::FSA_PRI_RAISE_N))
-      .addImm(EntryExitPri);
-
-  for (MachineBasicBlock &MBB : MF) {
-    if (!MBB.succ_empty())
-      continue;
-    insertLower(MBB, EntryExitPri);
-  }
+  unsigned EntryExitPri = insertLocalPriorityPairs(MF, ReconvBBSet);
+  insertEntryExitCompensation(MF, EntryExitPri);
 
   return true;
 }
